cb60d15be34a3fb6df87adf974ac919d20427cf5
Deanna Stevens <deanna.sofias@gmail.com>
2023-08-23 17:40:52 -0400

    Add meal.js inputs

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,42 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+}
+function createMenuItem(name, price, type) {
+  newMenuObject = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return newMenuObject;
+}
+function addIngredients(ingredient, array) {
+  if (!array.includes(ingredient)) {
+    array.push(ingredient);
+  }
+  return array;
+}
+function formatPrice(price) {
+  return `$${price}`;
+}
+function decreasePrice(price) {
+  var decreasedPrice = price - price * 0.1;
+  return decreasedPrice;
 }
 
+function createRecipe(title, ingredients, menuItemType) {
+  var recipeObject = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return recipeObject
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
