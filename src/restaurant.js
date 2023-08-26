2a217a7ccc18996ead5b4498e1c91c7c95ec524f
Deanna Stevens <deanna.sofias@gmail.com>
2023-08-25 23:00:58 -0400

    Add addMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -10,9 +10,44 @@ function createRestaurant(restaurant){
   return restaurant
 }
 
+// function addMenuItem(restaurant, menuItem){
+// console.log("restaurant--->", restaurant)
+// console.log("menuItem--->", menuItem)
+// for(var i = 0; i < )
+// if (menuItem.type === restaurant.menus[i]){
+
+// }
+// restaurant.menus.lunch.push(menuItem)
+
+// }
+
+// function addMenuItem(restaurant, menuItem) {
+//   if (!restaurant.menus[menuItem.type]) {
+//     restaurant.menus[menuItem.type] = []; // Create the menu array if it doesn't exist
+//   }
+  
+//   restaurant.menus[menuItem.type].push(menuItem);
+// }
+
+function addMenuItem(restaurant, menuItem) {
+  if (!restaurant.menus[menuItem.type]) {
+    restaurant.menus[menuItem.type] = [];
+  }
+  var exists = false;
+  for (var i = 0; i < restaurant.menus[menuItem.type].length; i++) {
+    if (restaurant.menus[menuItem.type][i].name === menuItem.name) {
+      exists = true;
+      break;
+    }
+  }
+  if (!exists) {
+    restaurant.menus[menuItem.type].push(menuItem);
+  }
+}
+
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
