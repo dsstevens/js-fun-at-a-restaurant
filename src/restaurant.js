184db7caf161c7fdb7d77f01af179ac597e38a27
Deanna Stevens <deanna.sofias@gmail.com>
2023-08-26 15:07:01 -0400

    Add removeMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -45,9 +45,23 @@ function addMenuItem(restaurant, menuItem) {
   }
 }
 
+function removeMenuItem(restaurant,itemName, menuItem){
+  // console.log("menus --->", restaurant.menus)
+  // console.log("menu item --->" , restaurant.menus[menuItem])
+  // console.log("item name --->",itemName)
+// confused why the 3rd arg on line 167 for "breakfast" comes out to be menuItem as a whole object in the param
+  var menuArray = restaurant.menus[menuItem];
+  for(var i = 0; i < menuArray.length; i++ ){
+    if (menuArray[i].type === menuItem) {
+      menuArray.splice(i,1);
+      return `No one is eating our ${itemName} - it has been removed from the ${menuItem} menu!`
+    }
+  } return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
+}
+
 module.exports = {
   createRestaurant, 
   addMenuItem,
-  // removeMenuItem,
+  removeMenuItem,
   // checkForFood
 }
