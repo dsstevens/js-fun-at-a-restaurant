d6d0ae264d08c7ed449c88c5f8abb5ac4a4adc39
Deanna Stevens <deanna.sofias@gmail.com>
2023-08-26 19:58:24 -0400

    Add checkForFood function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,33 +1,14 @@
-function createRestaurant(restaurant){
+function createRestaurant(restaurant) {
   var restaurant = {
     name: restaurant,
     menus: {
       breakfast: [],
       lunch: [],
-      dinner: []
-    }
-  }
-  return restaurant
+      dinner: [],
+    },
+  };
+  return restaurant;
 }
-
-// function addMenuItem(restaurant, menuItem){
-// console.log("restaurant--->", restaurant)
-// console.log("menuItem--->", menuItem)
-// for(var i = 0; i < )
-// if (menuItem.type === restaurant.menus[i]){
-
-// }
-// restaurant.menus.lunch.push(menuItem)
-
-// }
-
-// function addMenuItem(restaurant, menuItem) {
-//   if (!restaurant.menus[menuItem.type]) {
-//     restaurant.menus[menuItem.type] = []; // Create the menu array if it doesn't exist
-//   }
-  
-//   restaurant.menus[menuItem.type].push(menuItem);
-// }
 
 function addMenuItem(restaurant, menuItem) {
   if (!restaurant.menus[menuItem.type]) {
@@ -45,23 +26,29 @@ function addMenuItem(restaurant, menuItem) {
   }
 }
 
-function removeMenuItem(restaurant,itemName, menuItem){
-  // console.log("menus --->", restaurant.menus)
-  // console.log("menu item --->" , restaurant.menus[menuItem])
-  // console.log("item name --->",itemName)
-// confused why the 3rd arg on line 167 for "breakfast" comes out to be menuItem as a whole object in the param
+function removeMenuItem(restaurant, itemName, menuItem) {
   var menuArray = restaurant.menus[menuItem];
-  for(var i = 0; i < menuArray.length; i++ ){
+  for (var i = 0; i < menuArray.length; i++) {
     if (menuArray[i].type === menuItem) {
-      menuArray.splice(i,1);
-      return `No one is eating our ${itemName} - it has been removed from the ${menuItem} menu!`
+      menuArray.splice(i, 1);
+      return `No one is eating our ${itemName} - it has been removed from the ${menuItem} menu!`;
     }
-  } return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
+  }
+  return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+}
+
+function checkForFood(restaurant, itemObject) {
+  for (var i = 0; i < restaurant.menus[itemObject.type].length; i++) {
+    if (restaurant.menus[itemObject.type][i].name === itemObject.name) {
+      return `Yes, we're serving ${itemObject.name} today!`;
+    }
+  }
+  return `Sorry, we aren't serving ${itemObject.name} today.`;
 }
 
 module.exports = {
-  createRestaurant, 
+  createRestaurant,
   addMenuItem,
   removeMenuItem,
-  // checkForFood
-}
+  checkForFood,
+};
